1406e1f18444099878f6a84535bbdf1922efb7b2
poddnahh <77733265+poddnahh@users.noreply.github.com>
2025-06-15 20:13:45 -0500

    Update error_listener.js
    replaced.

--- Customize report colors and mode/js/error_listener.js
+++ Customize report colors and mode/js/error_listener.js
@@ -1,8 +1,6 @@
 // js/error_listener.js
-window.addEventListener("error", function(event) {
-  try {
-    if (window.parent.playground?.logShowcaseError) {
-      window.parent.playground.logShowcaseError("CustomizeColors", event);
-    }
-  } catch {}
+// ----------------------------------------------------------------------------
+window.addEventListener("error", e => {
+  console.error("Unhandled error:", e.error || e);
 });
+
